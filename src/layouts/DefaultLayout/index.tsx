0ea93be6be28baf0a71b865a2361a19b115c748c
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-05-22 23:37:52 -0300

    Feature: Create defaultLayout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { LayoutContainer } from "./styles";
+import { Header } from "../../components/Header";
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
+      <Header />
 
-      <Outlet/>
+      <main>
+        
+        <Outlet />
+      </main>
     </LayoutContainer>
   )
 }

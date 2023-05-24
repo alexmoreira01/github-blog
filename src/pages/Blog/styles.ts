9001583541dea9606374babd89d958441de73d51
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-05-24 00:23:35 -0300

    Feature: Add styles and components

--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -1,1 +1,17 @@
-// export const 
+import { styled } from "styled-components";
+
+export const PostsListContainer = styled.section`
+  width: 100%;
+
+  display: grid;
+
+  /* Duas Colunas */
+  /* grid-template-columns: 1fr 1fr; */
+
+  /* Se ajustar quando o tamanho da tela mudar -- no minimo ira ocupar 26rem 3 no maximo 1 linha inteira */
+  grid-template-columns: repeat(auto-fit, minmax(26rem, 1fr));
+
+  gap: 2rem;
+
+  margin-bottom: 14rem;
+`;
